2f7aee6b006503b04a3fbbd60a1d31d41cdfedbd
Roland Levy <rolandjlevy@gmail.com>
2020-10-19 23:27:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Set up server with express.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set up ejs templates
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.locals.uri = req.originalUrl;
+  next();
+});
+
+// Set up routes
+app.get('/', function(req, res) {
+  res.render('pages/index', { title: 'Home', uri: res.locals.uri });
+});
+
+app.get('/about', function(req, res) {
+  res.render('pages/about', { title: 'About', uri: res.locals.uri });
+});
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
