6c11efaeafd192c0075dedea9922f1621e815339
Roland Levy <rolandjlevy@gmail.com>
2020-11-14 19:46:43 +0000

    Refactored

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.locals.uri = req.originalUrl;
   next();
